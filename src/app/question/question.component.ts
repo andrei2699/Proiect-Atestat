1c64e05df8674711fbbc50f598eb838fabc0bccc
Andrei Timar <atimar999@gmail.com>
2017-11-28 19:55:58 +0200

    Updates

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewEncapsulation ,Inject} from '@angular/core';
-import {QUESTIONS} from '../fake-questions';
+import { Component, OnInit, ViewEncapsulation, Inject } from '@angular/core';
+import { QUESTIONS } from '../fake-questions';
 import { question } from '../question';
 import { Router, NavigationStart, Event as NavigationEvent } from '@angular/router';
 import { DataService } from '../data.service';
@@ -15,42 +15,39 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 export class QuestionComponent implements OnInit {
 
-  constructor(private router: Router,private _myService: DataService,public dialog: MatDialog){}
-  response=[];
+  constructor(private router: Router, private _myService: DataService, public dialog: MatDialog) { }
+  response = [];
   SharedData: number;
-  questions=QUESTIONS;
-  
-  ngOnInit() {}
-  
-   alert(){
-    let Rez=0;
+  questions = QUESTIONS;
+
+  ngOnInit() { }
+
+  alert() {
+    let Rez = 0;
     let g;
-    for(let i=0;i<=9;i++)
-    {
-      if(this.response[i]==this.questions[1].correct)
-      {
-           Rez++;     
-      } 
+    for (let i = 0; i <= 9; i++) {
+      if (this.response[i] == this.questions[1].correct) {
+        Rez++;
+      }
     }
     this._myService.setData(Rez);
-    this.SharedData=Rez;
+    this.SharedData = Rez;
     console.log(this._myService.getData());
 
     const dialogRef = this.dialog.open(ConfirmDialog, {
       width: '400px',
-      data: {message: ' ',title: 'Esti sigur ca vrei sa trimiti testul ?' }
+      data: { message: ' ', title: 'Esti sigur ca vrei sa trimiti testul ?' }
     });
 
     dialogRef.afterClosed().subscribe(result => {
       console.log(result);
-      g=result;
-      if(g==true)
-      {
-         this.GoTo("result");
+      g = result;
+      if (g == true) {
+        this.GoTo("result");
       }
     });
   }
- public GoTo(a :string){
-    this.router.navigateByUrl('/'+a);
+  public GoTo(a: string) {
+    this.router.navigateByUrl('/' + a);
   }
 }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,7 +3,7 @@ import { DataService } from '../data.service';
 import { Router, NavigationStart, Event as NavigationEvent } from '@angular/router';
 import { TestsService } from '../tests.service';
 import { test } from '../test';
-import {Injectable, Pipe, PipeTransform} from '@angular/core';
+import { Injectable, Pipe, PipeTransform } from '@angular/core';
 
 @Component({
   selector: 'app-test',
@@ -11,36 +11,37 @@ import {Injectable, Pipe, PipeTransform} from '@angular/core';
   styleUrls: ['./test.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class TestComponent implements OnInit{
+export class TestComponent implements OnInit {
 
-  constructor(private router: Router,private _myService: DataService,private testsService: TestsService) { 
-  console.log(this._myService.getData());
+  constructor(private router: Router, private _myService: DataService, private testsService: TestsService) {
+    console.log(this._myService.getData());
   }
 
   tests: test[];
 
   getTests(): void {
     this.testsService.getTests()
-        .subscribe(tests => this.tests = tests);
+      .subscribe(tests => this.tests = tests);
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.getTests();
-  };
-  show(a:string) { 
-    let e =  document.getElementById(a).getElementsByTagName("button");
-      for(let i=0;i<=1;i++){
-        e[i].style.display = "initial";
-       }
-   }     
-  hide(a:string) { 
-    let e =  document.getElementById(a).getElementsByTagName("button");
-      for(let i=0;i<=1;i++){
-        e[i].style.display = "none"; 
-      }
-    }
+  }
 
-GoTo(a :string){
+  show(a: string) {
+    let e = document.getElementById(a).getElementsByTagName("button");
+    for (let i = 0; i <= 1; i++) {
+      e[i].style.display = "initial";
+    }
+  }
+  hide(a: string) {
+    let e = document.getElementById(a).getElementsByTagName("button");
+    for (let i = 0; i <= 1; i++) {
+      e[i].style.display = "none";
+    }
+  }
+
+  GoTo(a: string) {
     this.router.navigateByUrl('questions');
-}
+  }
 }
